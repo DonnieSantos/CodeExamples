093d62f1689b5704554947c6d91605f952d2a1d3
Donald J. Santos <DonaldJSantos@Gmail.com>
2016-11-14 21:09:15 -0500

    The Office

---
+++ The Office/CSharp/TheOffice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Timers;
+
+namespace TheOffice
+{
+    public class Program
+    {
+        public static Map Map;
+        public static Timer Timer;
+        public static Office Office;
+
+        public static void Main(string[] args)
+        {
+            Map = new Map();
+            Office = new Office();
+            Timer = new Timer();
+            Timer.AutoReset = true;
+            Timer.Interval = 1000;
+            Timer.Elapsed += new ElapsedEventHandler(TickTock);
+            Timer.Start();
+            while (true) { }
+        }
+
+        public static void TickTock(object o, ElapsedEventArgs e)
+        {
+            Office.MoveWorkers();
+            Map.Display(Office.Rooms);
+        }
+    }
+}
